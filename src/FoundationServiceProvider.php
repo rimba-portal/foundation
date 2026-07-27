a14867b4ea24c6cb2f1d9f079e6ab48ce4de211f
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-27 10:12:40 +0800

    Update 2026-07-27 10:12

---
+++ src/FoundationServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Foundation;
+
+use Rimba\Base\Services\BitesServiceProvider;
+use Illuminate\Console\Command;
+use ReflectionClass;
+
+
+class FoundationServiceProvider extends BitesServiceProvider
+{
+    // protected string $configFile = __DIR__ . '/../config/bites.php';
+
+    protected function bootPackage(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->registerCommandsFromDirectory();
+        }
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+    /**
+     * Dynamically discover and boot all commands inside the package directory.
+     */
+    protected function registerCommandsFromDirectory()
+    {
+        $commandDir = __DIR__ . '/Console/Commands';
+        if (! is_dir($commandDir)) {
+            return;
+        }
+        $commands = [];
+        foreach (glob($commandDir . '/*.php') as $file) {
+            $className = basename($file, '.php');
+            $class = 'Rimba\\Base\\Console\\Commands\\' . $className;
+            if (class_exists($class) && is_subclass_of($class, Command::class)) {
+                $reflection = new ReflectionClass($class);
+                if (! $reflection->isAbstract()) {
+                    $commands[] = $class;
+                }
+            }
+        }
+        if ($commands !== []) {
+            $this->commands($commands);
+        }
+    }
+}
