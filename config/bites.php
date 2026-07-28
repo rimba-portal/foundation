4c1e110ee3e8c6bf571007d035b5c48b4a21ed1c
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-28 13:54:47 +0800

    Release v1.2.5

--- config/bites.php
+++ config/bites.php
@@ -8,8 +8,8 @@ return [
         'panels' => [
             // panel_id, path, color, brandName, homeUrl
             'admin' => ['admin', 'admin', '#7f174b', 'Administrator Portal', 'filament.staff.pages.dashboard'],
-            'lobby' => ['lobby', 'lobby', '#069800', 'ATM Lobby', 'welcome'],
-            'staff' => ['staff', 'staff', '#09829f', 'ATM Staff Intranet', 'welcome'],
+            'lobby' => ['lobby', 'lobby', '#069800', 'ATM Lobby', '/'],
+            'staff' => ['staff', 'staff', '#09829f', 'ATM Staff Intranet', 'filament.staff.pages.dashboard'],
         ],
     ],
     'unit_roles' => ['owner','member'],
